f78f6288df592aaa18ac38bb691757d263cd58b8
kaydenireland <kaydenireland@aol.com>
2025-11-12 23:02:48 -0500

    Blocks/Unblocks Websites based on whether an IDE is loading

--- FocusCoding/Program.cs
+++ FocusCoding/Program.cs
@@ -12,7 +12,9 @@ namespace FocusCoding
         // List of sites to block
         private static readonly string[] blockedSites =
         {
+        "chatgpt.com",
         "www.chatgpt.com",
+        "youtube.com",
         "www.youtube.com"
         };
 
@@ -22,10 +24,35 @@ namespace FocusCoding
             "Code" // VSCode
         };
 
+        private static readonly string newIP = "127.0.0.1";
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine(ShouldBlock());
+            Console.WriteLine("Starting FocusCoding Blocker");
+            bool currentlyBlocking = false;
+            while (true)
+            {
+                try {
+                    if (!currentlyBlocking && ShouldBlock())
+                    {
+                        currentlyBlocking = true;
+                        Console.WriteLine("IDE Running, Blocking Sites.");
+                        BlockSites();
+                    }
+                    else if (currentlyBlocking && !ShouldBlock())
+                    {
+                        currentlyBlocking = false;
+                        Console.WriteLine("No IDEs Running, Unblocking Sites.");
+                        UnblockSites();
+                    }
+                } 
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+                Thread.Sleep(15000);    // 30000 = 30 seconds
+            }
         }
 
         private static bool IsRunning(string process)
@@ -35,14 +62,40 @@ namespace FocusCoding
 
         private static bool ShouldBlock()
         {
-            for(int i = 0; i < processes.Length; i++)
+            foreach (string p in processes)
             {
-                if (IsRunning(processes[i]))
+                if (IsRunning(p))
                 {
                     return true;
                 }
             }
             return false;
+        }
+
+        private static void BlockSites()
+        {
+            var content = File.ReadAllText(hostsPath);
+            var newLines = new List<string>();
+
+            foreach (var site in blockedSites)
+            {
+                string entry = $"{newIP} {site}";
+                if (!content.Contains(entry))
+                    newLines.Add(entry);
+            }
+
+            if (newLines.Count > 0)
+                File.AppendAllLines(hostsPath, newLines);
+        }
+
+
+        private static void UnblockSites()
+        {
+            var lines = File.ReadAllLines(hostsPath)
+                .Where(line => !blockedSites.Any(site => line.Contains(site)))
+                .ToList();
+
+            File.WriteAllLines(hostsPath, lines);
         }
     }
 }
